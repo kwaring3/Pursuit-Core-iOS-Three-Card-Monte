36697f33b9ab8675568ac1f79af1f33355c3530a
kwaring3 <kevinwaring@pursuit.org>
2018-11-07 12:34:17 -0500

    First Part Done

--- ThreeCardMonte/ThreeCardMonte/ViewController.swift
+++ ThreeCardMonte/ThreeCardMonte/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+var correct = 0
+var incorrect = 0
 class ViewController: UIViewController {
     
     
@@ -15,7 +17,7 @@ class ViewController: UIViewController {
     
 ///////Label output
     @IBOutlet weak var mainLabel: UILabel!
-    
+    @IBOutlet weak var messageLabel: UILabel!
     //////Buttons
     
     @IBOutlet weak var card1: UIButton!
@@ -43,32 +45,71 @@ class ViewController: UIViewController {
       
         let cardArray: [Int] = [0,1,2]
         let randomCard =  cardArray.randomElement()
+        //messageLabel.text = "Choose your Card"
         switch sender.tag {
         case 0:
             if randomCard == sender.tag {
-            
             card1.setImage(UIImage.init(named: "kingCard"), for: .normal)
-                card2.isEnabled = false
-                card3.isEnabled = false
-            } else {
+                messageLabel.text = "You got me"
+                correct += 1
+            } else if randomCard == 2  {
                 
                 card1.setImage(UIImage.init(named: "threeCard"), for: .normal)
                 card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
-                card3.isEnabled = false
+                messageLabel.text = "Give me my money please"
+                incorrect += 1
+            } else {
+                card1.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                
+                card3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                messageLabel.text = "Give me my money please"
+                incorrect += 1
             }
-            
+            card1.isEnabled = false
+            card2.isEnabled = false
+            card3.isEnabled = false
         case 1:
             if randomCard == sender.tag {
-            card1.setImage(UIImage.init(named: "kingCard"), for: .normal)
-            card2.setImage(UIImage.init(named: "threeCard"), for: .normal)
-            card3.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                messageLabel.text = "You got me"
+                correct += 1
+            } else if randomCard == 2  {
+                
+                card2.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                card3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                messageLabel.text = "Give me my money please"
+                incorrect += 1
+            } else {
+                card2.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                
+                card1.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                messageLabel.text = "Give me my money please"
+                incorrect += 1
             }
+            card1.isEnabled = false
+            card2.isEnabled = false
+            card3.isEnabled = false
         case 2:
-                if randomCard == sender.tag {
-            card1.setImage(UIImage.init(named: "threeCard"), for: .normal)
-            card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
-            card3.setImage(UIImage.init(named: "threeCard"), for: .normal)
-                }
+            if randomCard == sender.tag {
+                card3.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                messageLabel.text = "You got me"
+                correct += 1
+            } else if randomCard == 2  {
+                
+                card3.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                card1.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                messageLabel.text = "Give me my money please"
+                incorrect += 1
+            } else {
+                card3.setImage(UIImage.init(named: "threeCard"), for: .normal)
+                
+                card2.setImage(UIImage.init(named: "kingCard"), for: .normal)
+                messageLabel.text = "Give me my money please"
+                incorrect += 1
+            }
+            card1.isEnabled = false
+            card2.isEnabled = false
+            card3.isEnabled = false
         default:
             print("invalid tag")
         }
